735728a7c0d7a4e4e1a38c6f7858a0a25551d9c3
sleepy <xhadley@gmail.com>
2023-12-08 15:11:17 -0600

    testing torn layout on helix also reverting number layout change

--- helix/keymap.c
+++ helix/keymap.c
@@ -53,14 +53,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  | Enter|
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * | Esc  |      |      | Alt  | Lower| Nav  | Space| Win  | Space| Raise| Prev | Play | Next | Mute |
+   * |      |      |      | Alt  | Nav  | Lower| Space| Esc  | Space| Raise| Win  |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-      KC_ESC,  XXXXXXX, XXXXXXX, KC_LALT, LOWER,   NAV,     KC_SPC,  KC_LGUI, KC_SPC,  RAISE,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE  \
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, NAV,     LOWER,   KC_SPC,  KC_ESC,  KC_SPC,  RAISE,   KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX  \
       ),
 
   /* Nav
@@ -69,34 +69,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|      | Left | Down | Right|      |             |      | End  | PgDn |PrntSc|      | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |      |      |      |      |      |             |      |      |      |      |      | Enter|
+   * | Ctrl | Mute | Prev | Play | Next |      |             |      |      |      |      |      | Enter|
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * | Esc  |      |      | Alt  | Lower| Nav  | Space| Win  | Space| Raise| Prev | Play | Next | Mute |
+   * |      |      |      | Alt  | Nav  | Lower| Space| Esc  | Space| Raise| Win  |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_NAV] = LAYOUT( \
       _______, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,                   XXXXXXX, KC_HOME, KC_PGUP, KC_INS,  XXXXXXX, _______, \
       _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                   XXXXXXX, KC_END,  KC_PGDN, KC_PSCR, XXXXXXX, KC_DEL,  \
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+      _______, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
       ),
 
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Tab  |   -  |   1  |   2  |   3  |   +  |             |      |   !  |   @  |   #  |   $  | Bksp |
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |             |      |   !  |   @  |   #  |   $  | Bksp |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Shift|   *  |   4  |   5  |   6  |   .  |             |      |   =  |   ^  |   %  |   `  | Del  |
+   * | Shift|   6  |   7  |   8  |   9  |   0  |             |      |   =  |   ^  |   %  |   `  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |   /  |   7  |   8  |   9  |   0  |             |      |      |      |      |      | Enter|
+   * | Ctrl |   /  |   *  |   -  |   +  |   .  |             |      |      |      |      |      | Enter|
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * | Esc  | Enter|      | Alt  | Lower| Nav  | Space| Win  | Space| Raise| Prev | Play | Next | Mute |
+   * |      |      |      | Alt  | Nav  | Lower| Space| Esc  | Space| Raise| Win  |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-      _______, KC_MINS, KC_1,    KC_2,    KC_3,    KC_PLUS,                   XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, \
-      _______, KC_ASTR, KC_4,    KC_5,    KC_6,    KC_DOT,                    XXXXXXX, KC_EQL,  KC_CIRC, KC_PERC, KC_GRV,  KC_DEL,  \
-      _______, KC_SLSH, KC_7,    KC_8,    KC_9,    KC_0,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-      _______, KC_ENT,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, \
+      _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      XXXXXXX, KC_EQL,  KC_CIRC, KC_PERC, KC_GRV,  KC_DEL,  \
+      _______, KC_SLSH, KC_ASTR, KC_MINS, KC_PLUS, KC_DOT,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
       ),
 
   /* Raise
@@ -107,14 +107,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |  F9  |  F10 |  F11 |  F12 |      |             |      |   [  |   ]  |      |      | Enter|
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * | Esc  |      |      | Alt  | Lower| Nav  | Space| Win  | Space| Raise| Prev | Play | Next | Mute |
+   * |      |      |      | Alt  | Nav  | Lower| Space| Esc  | Space| Raise| Win  |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,                   XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, KC_BSLS, _______, \
       _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,                   XXXXXXX, KC_LCBR, KC_RCBR, KC_AMPR, KC_PIPE, KC_DEL,  \
       _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,                   XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, _______, \
-      _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
       ),
 
   /* Adjust (Lower + Raise)
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |      |      |      |      |      |             |      |      |      |      |      |      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      |      |      |      | Lower| Nav  |      |      |      | Raise|      |      |      | Flash|
+   * |      |      |      |      | Nav  | Lower|      |      |      | Raise|      |      |      | Flash|
    * `-------------------------------------------------------------------------------------------------'
    */
   [_ADJUST] =  LAYOUT( \
